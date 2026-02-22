9738973daeb4eb67998734d42c72749575c293be
Sullyvan Ogawa <sullyvanogawa@gmail.com>
2026-02-21 23:42:09 -0300

    Adicionando as páginas HTML sem estilização.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 
 import express from 'express';
 
-const host = "0.0.0.0";
+const host = "127.0.0.1";
 const porta = 3001;
 const app = express();
 
-app.listen(host, porta, () => {
-    console.log(`Servidor rodando ${host}: ${porta}`);
+// app.use(express.static('./Views/public'));
+
+app.listen(porta, host, () => {
+    console.log(`Servidor rodando http://${host}:${porta}`);
 });
